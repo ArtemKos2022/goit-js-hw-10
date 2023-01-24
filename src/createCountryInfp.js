a5909149c18e7e43f95cdc90f2fa248440f00998
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2023-01-24 23:20:05 +0200

    task

---
+++ src/createCountryInfp.js
@@ -0,0 +1,8 @@
+export default function createCountryInfo(country) {
+  return country
+    .map(({ capital, population, languages }) => {
+      const languagesName = languages.map(({ name }) => name).join(', ');
+      return `<p>Capital: ${capital}</p>Population: ${population}<p></p><p>Languages: ${languagesName}</p>`;
+    })
+    .join('');
+}
